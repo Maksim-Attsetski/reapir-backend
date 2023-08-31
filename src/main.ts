b0597ab11aa4f0e01b9d81ed1bdb06dbab2097d9
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-08-31 18:02:53 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import { AppModule, Config } from './modules';
+import { AppModule } from './modules';
 
 const PORT = process.env.PORT || 3000;
 
 async function loadServer() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(Config.corsOptions);
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://repair-eta.vercel.app'],
+  });
   app.use(cookieParser('secret_cookie'));
   app.setGlobalPrefix('/api');
 
