786a259978f1f6ad4c5bce9bd6e812cffe2a0aa3
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-09-01 15:25:54 +0300

    update cors origin

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,7 @@ async function loadServer() {
 
   app.enableCors({
     credentials: true,
-    // origin: ['http://localhost:3000', 'https://repair-eta.vercel.app'],
-    origin: 'https://repair-eta.vercel.app',
+    origin: ['http://localhost:3000', 'https://repair-eta.vercel.app'],
   });
   app.use(cookieParser('secret_cookie'));
   app.setGlobalPrefix('/api');
